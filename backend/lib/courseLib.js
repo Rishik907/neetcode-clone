94c2c5dda8f979a40ad2d3b1fc143b19675dd8de
Rishik907 <nakkarishiks@gmail.com>
2023-02-26 15:11:22 +0530

    create update getallcourses

--- backend/lib/courseLib.js
+++ backend/lib/courseLib.js
@@ -4,6 +4,25 @@ module.exports.getAllcourses = async function(){
     return await courseModel.find({});
 }
 
+module.exports.createCourse = async function(courseInput){
+    const course = new courseModel(courseInput);
+    await course.save();
+    // const courses = await courseModel.find({});
+    // if (courses && courses.length ==0){
+        
+    //     const firstCourseInput = {
+    //         "title": "bz DSA", 
+    //         "level" : "easy",
+    //     };
+    //     const course = new courseModel(firstCourseInput);
+    //     await course.save();
+    // }
+}
+
+module.exports.updateCourse = async function(courseInput){
+    await courseModel.findOneAndUpdate({_id: courseId}, updatedInput);
+
+}
 
 module.exports.createFirstCourse = async function(){
     const courses = await courseModel.find({});
