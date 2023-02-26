c078220727352f8001c10b70b2a9ae1a4e05e03b
Rishik907 <nakkarishiks@gmail.com>
2023-02-26 14:14:54 +0530

    adding mongodb to project

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const str = process.env.MONGO_CONNECTION_STRING;
+
+const mongoose = require('mongoose');
+
 
 const app = express();
 
@@ -7,9 +13,20 @@ app.use(express.static("frontend"))
 app.get("/",function(req,res){
     res.sendFile(__dirname +"/frontend/index.html");
 })
-
-
-
-app.listen(3000,function(){
-    console.log("server running on http://localhost:3000")
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,function(err){
+    if(err){
+        console.error(err);
+    }
+    else{
+        console.log("DB Connected Successful");
+        app.listen(3000,function(){
+            console.log("server running on http://localhost:3000")
+        })
+        //TODO: start express app server here
+    }
 })
+
+// app.listen(3000,function(){
+//     console.log("server running on http://localhost:3000")
+// })
